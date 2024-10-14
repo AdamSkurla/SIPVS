ca8b03ca2ee06517b6cbcfe97b901c8e93a2d14d
Adam Jankanič <xjankanic@stuba.sk>
2024-10-14 11:15:35 +0200

    FINAL - ASiCE container, Bp object

--- obj/Debug/net8.0/SIPVS.AssemblyInfo.cs
+++ obj/Debug/net8.0/SIPVS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIPVS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+151d41c45fce8f40c67c6cf71c24527eb1e48ca9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875b6643aae0eae3a3af99b7a7ead0ab957f86ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIPVS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIPVS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
